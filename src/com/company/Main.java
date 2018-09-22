a54b6ad137a35d8b2b60401f73a5de4f1b392854
egorsni <43299958+egorsni@users.noreply.github.com>
2018-09-22 15:19:56 +0500

    уменьшил код

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,19 +19,19 @@ public class Main {
         }
         if ((b > 0) & (b <= 8)) {
             System.out.println("byte short int long");
-            n = 1;
+            n = 8;
         }
         if ((b > 8) & (b <= 16)) {
             System.out.println("short int long");
-            n = 2;
+            n = 16;
         }
         if ((b > 16) & (b <= 32)) {
             System.out.println("int long");
-            n = 3;
+            n = 32;
         }
         if ((b > 32) & (b <= 64)) {
             System.out.println("long");
-            n = 4;
+            n = 64;
         }
         if (a >= 0) {
             int[] c;
@@ -47,8 +47,7 @@ public class Main {
             }
         }
         if (a < 0) {
-            if (n == 1) {
-                               for (i=0;i<(8-b);i++) {
+                               for (i=0;i<(n-b);i++) {
                                 System.out.print("1");
                }
                 d = b;
@@ -79,102 +78,5 @@ public class Main {
                     }
 
                 }
-            if (n == 2) {
-                for (i=0;i<(16-b);i++) {
-                    System.out.print("1");
-                }
-                d = b;
-                int[] c;
-                y = Math.abs(a);
-                c = new int[b+1];
-                while ((y > 1) & (d>=0)){
-                    c[d] =1 - y % 2;
-                    d = d - 1;
-                    y = y / 2;
-                }
-                switch (c[b]) {
-                    case 0:
-                        c[b] =1;
-                        break;
-                    case 1:
-                        c[b] = 2;
-                        break;
-                }
-                for (i=b;i>1;i--)
-                    if(c[i] == 2){
-                        c[i] = 0;
-                        c[i-1] = c[i-1] +1;
-                    }
-
-                for (i = 1; i <= b; i++) {
-                    System.out.print(c[i]);
-                }
-
-            }
-            if (n == 3) {
-                for (i=0;i<(32-b);i++) {
-                    System.out.print("1");
-                }
-                d = b;
-                int[] c;
-                y = Math.abs(a);
-                c = new int[b+1];
-                while ((y > 1) & (d>=0)){
-                    c[d] =1 - y % 2;
-                    d = d - 1;
-                    y = y / 2;
-                }
-                switch (c[b]) {
-                    case 0:
-                        c[b] =1;
-                        break;
-                    case 1:
-                        c[b] = 2;
-                        break;
-                }
-                for (i=b;i>1;i--)
-                    if(c[i] == 2){
-                        c[i] = 0;
-                        c[i-1] = c[i-1] +1;
-                    }
-
-                for (i = 1; i <= b; i++) {
-                    System.out.print(c[i]);
-                }
-
-            }
-            if (n == 4) {
-                for (i=0;i<(64-b);i++) {
-                    System.out.print("1");
-                }
-                d = b;
-                int[] c;
-                y = Math.abs(a);
-                c = new int[b+1];
-                while ((y > 1) & (d>=0)){
-                    c[d] =1 - y % 2;
-                    d = d - 1;
-                    y = y / 2;
-                }
-                switch (c[b]) {
-                    case 0:
-                        c[b] =1;
-                        break;
-                    case 1:
-                        c[b] = 2;
-                        break;
-                }
-                for (i=b;i>1;i--)
-                    if(c[i] == 2){
-                        c[i] = 0;
-                        c[i-1] = c[i-1] +1;
-                    }
-
-                for (i = 1; i <= b; i++) {
-                    System.out.print(c[i]);
-                }
-
-            }
             }
         }
-    }
